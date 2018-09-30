5a41c251dbe0c5304b67539b22a64372d2eafc30
ucing <you@example.com>
2018-09-30 09:34:24 +0700

    Remove Comment
    Remove Comment

---
+++ js/dragdrop/column.js
@@ -0,0 +1,1 @@
+const DDColumn = new DDContainer();
--- js/dragdrop/draggable.js
+++
@@ -1,9 +0,0 @@
-class DDDraggable extends DDViewAbstract {
-
-    static bind () {
-
-    };
-}
-
-
-DD.document.panels['row'] = (new DDRowPanel).render();
