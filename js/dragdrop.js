5866e7f6511b3b9eadabc015694d989dabd3f034
ucing <you@example.com>
2018-10-01 23:56:48 +0700

    Remove Comment
    Remove Comment

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -3,269 +3,398 @@
  *
  */
 
-class DDAttribute {
 
-    constructor(list = {}, named = {}) {
+import a from './dragdrop/component/a';
 
-        this.$associative = named;
-        this.$list = list;
-    }
-
-    /**
-     * @param name
-     * @returns {array}
-     */
-    list(name) {
-
-        if(!this.$list.hasOwnProperty(name)) {
-
-            this.$list[name] = [];
-        }
-
-        return this.$list[name];
-    }
-
-    /**
-     *
-     * @param name
-     * @returns {object}
-     */
-    associative(name) {
-
-        if(!this.$associative.hasOwnProperty(name)) {
-
-            this.$associative[name] = {};
-        }
-
-        return this.$associative[name];
-    }
-
-    toString() {
-
-        var object = {};
-
-        for (var key in this.$associative) {
-
-            object[key] = Object.values(this.$associative[key]);
-        }
-
-        for(key in this.$list) {
-
-            if(!(key in object)) {
-
-                object[key] = [];
-            }
-
-            object[key].push(...this.$list[key]);
-        }
-
-        var attributes = [];
-
-        for (key in object) {
-
-            var value = Object.values(object[key]).join(' ');
-
-            attributes.push(`${key}="${value}"`);
-        }
-
-        return attributes.join(' ');
-
-    }
-}
+// class A {
+//
+//     constructor($a) {
+//
+//         this.a = $a;
+//     }
+// }
+//
+// class B {
+//
+//     constructor($b) {
+//
+//         var a;
+//         this.b = $b;
+//     }
+//
+//     zz () {
+//
+//         return a;
+//     }
+// }
+//
+// class C extends A, B {
+//
+//     constructor() {
+//
+//         super();
+//     }
+// }
 
 
-class DDElementAbstract  {
+function DDElement(content, tag = 'div', attribute = new DDAttribute()) {
 
-    constructor(attribute = new DDAttribute(), tag = 'div') {
-
-        console.assert(attribute instanceof DDAttribute);
-        console.assert(typeof  tag === 'string');
-
-        this.tag = tag;
-        //this.$bind = bind;
-        this.attribute = attribute;
+    console.assert(attribute instanceof DDAttribute);
+    console.assert(typeof  tag === 'string');
 
 
-    }
+    this.attribute = attribute;
+    this.tag = tag;
 
-
-    inner () {
-
-        return '';
-    }
-
-    toString () {
-
-        return `<${this.tag} ${this.attribute}>${this.inner()}</${this.tag}>`;
-    }
-}
-
-class DDElementBind extends DDElementAbstract{
-
-    constructor(attribute = new DDAttribute(), tag = 'div') {
-
-        console.assert(attribute instanceof DDAttribute);
-        console.assert(typeof  tag === 'string');
-
-        super(attribute, tag);
-        attribute.associative('class')[DDContainer.identifier()] = DDContainer.identifier();
-    }
-
-    static identifier(selector = false) {
-
-        return selector ? '.' + this.name : this.name ;
-    }
-
-    // bind(selector = false) {
-    //
-    //     if(!this.$bind) {
-    //
-    //         throw new Error('bind is not set');
-    //     }
-    //
-    //     return selector ? '.' + this.$bind : this.$bind ;
-    // }
-
-
-    setTo(jquery) {
-
-        jquery.addClass(this.bind());
-    }
-}
-
-class DDElement extends DDElementAbstract {
-
-    constructor(content = '', attribute = new DDAttribute(), tag = 'div') {
-
-        super(attribute, tag);
-        this.content = content;
-    }
-
-    inner () {
+    this.content = function () {
 
         return this.content;
-    }
+    };
 
+    this.toString = function() {
+
+        return `<${this.tag} ${this.attribute}>${this.content()}</${this.tag}>`;
+    };
+}
+
+function DDElementBind(element = new DDElement()) {
+
+    this.__proto__ = element;
+
+    this.identifier = function(selector = false) {
+
+        return selector ? '.' + object.constructor.name : object.constructor.name ;
+    };
+
+    this.bind = function(selector = false) {
+
+        return selector ? '.' + bind : bind ;
+    };
+
+    object.setTo = function(jquery) {
+
+        jquery.addClass(object.constructor.identifier());
+    };
+
+    object.attribute.named('class')[object.identifier()] = object.identifier();
+    object.attribute.named('class')[bind] = bind;
+}
+
+
+function DDContent(object) {
+
+    object.content = content;
+
+    object.content = function () {
+
+        return this.content;
+    };
 }
 
 
 
-class DDContainer extends DDElementBind {
+//
+// class DDAttribute {
+//
+//     constructor(list = {}, named = {}) {
+//
+//         this.$associative = named;
+//         this.$list = list;
+//     }
+//
+//     /**
+//      * @param name
+//      * @returns {array}
+//      */
+//     list(name) {
+//
+//         if(!this.$list.hasOwnProperty(name)) {
+//
+//             this.$list[name] = [];
+//         }
+//
+//         return this.$list[name];
+//     }
+//
+//     /**
+//      *
+//      * @param name
+//      * @returns {object}
+//      */
+//     associative(name) {
+//
+//         if(!this.$associative.hasOwnProperty(name)) {
+//
+//             this.$associative[name] = {};
+//         }
+//
+//         return this.$associative[name];
+//     }
+//
+//     toString() {
+//
+//         var object = {};
+//
+//         for (var key in this.$associative) {
+//
+//             object[key] = Object.values(this.$associative[key]);
+//         }
+//
+//         for(key in this.$list) {
+//
+//             if(!(key in object)) {
+//
+//                 object[key] = [];
+//             }
+//
+//             object[key].push(...this.$list[key]);
+//         }
+//
+//         var attributes = [];
+//
+//         for (key in object) {
+//
+//             var value = Object.values(object[key]).join(' ');
+//
+//             attributes.push(`${key}="${value}"`);
+//         }
+//
+//         return attributes.join(' ');
+//
+//     }
+// }
+//
+//
+// class DDElementAbstract  {
+//
+//     constructor(attribute = new DDAttribute(), tag = 'div') {
+//
+//         console.assert(attribute instanceof DDAttribute);
+//         console.assert(typeof  tag === 'string');
+//
+//         this.tag = tag;
+//         this.attribute = attribute;
+//     }
+//
+//     inner () {
+//
+//         return '';
+//     }
+//
+//     toString () {
+//
+//         return `<${this.tag} ${this.attribute}>${this.inner()}</${this.tag}>`;
+//     }
+// }
+//
+// class DDElementBind extends DDElementAbstract{
+//
+//     constructor(bind = '', attribute = new DDAttribute(), tag = 'div') {
+//
+//         console.assert(attribute instanceof DDAttribute);
+//         console.assert(typeof  tag === 'string');
+//
+//         super(attribute, tag);
+//         attribute.associative('class')[this.constructor.identifier()] = this.constructor.identifier();
+//
+//         this.$bind = bind;
+//
+//         if(bind) {
+//
+//             attribute.associative('class')[bind] = bind;
+//         }
+//     }
+//
+//     static identifier(selector = false) {
+//
+//         return selector ? '.' + this.name : this.name ;
+//     }
+//
+//     bind(selector = false) {
+//
+//         if(!this.$bind) {
+//
+//             throw new Error('bind is not set');
+//         }
+//
+//         return selector ? '.' + this.$bind : this.$bind ;
+//     }
+//
+//
+//     setTo(jquery) {
+//
+//         jquery.addClass(this.constructor.identifier());
+//     }
+// }
+//
+// class DDElement extends DDElementAbstract {
+//
+//     constructor(content = '', attribute = new DDAttribute(), tag = 'div') {
+//
+//         super(attribute, tag);
+//         this.content = content;
+//     }
+//
+//     inner () {
+//
+//         return this.content;
+//     }
+//
+// }
+//
 
-    constructor(attribute = new DDAttribute(), panel = new DDPanel(), tag = 'div') {
+function DDContainer(bind = '', element = new DDElement()) {
 
-        super(attribute, tag);
-        this.panel = panel;
+    this.__proto__ = element;
 
-       // attribute.associative('class')[DDContainer.identifier()] = DDContainer.identifier();
+    console.assert(element instanceof DDElement);
 
-    }
-
-    static from(jquery) {
-
-        if(!jquery.hasClass(this.identifier())) {
-
-            jquery = jquery.parents(this.identifier(true)).first();
-        }
-
-        return jquery;
-    }
-
-    setTo(jquery) {
-
-        super.setTo(jquery);
-        this.panel.setTo(jquery);
-    }
+    DDElementBind(this, bind);
+    DDPanel(this);
 }
 
-class DDItems extends DDElementAbstract {
+//
+// class DDContainer extends DDElementBind {
+//
+//     constructor(bind = '', attribute = new DDAttribute(), panel = new DDPanel(), tag = 'div') {
+//
+//         super(bind, attribute, tag);
+//         this.panel = panel;
+//
+//        // attribute.associative('class')[DDContainer.identifier()] = DDContainer.identifier();
+//
+//     }
+//
+//     static from(jquery) {
+//
+//         if(!jquery.hasClass(this.identifier())) {
+//
+//             jquery = jquery.parents(this.identifier(true)).first();
+//         }
+//
+//         return jquery;
+//     }
+//
+//     setTo(jquery) {
+//
+//         super.setTo(jquery);
+//         this.panel.setTo(jquery);
+//     }
+// }
 
-    constructor(attribute = new DDAttribute(), items = {}, tag = 'div') {
+function DDItems(object, attribute = new DDAttribute()) {
 
-        super(attribute, tag);
-        this.items = items;
-    }
+    object.items = {};
 
-    inner() {
+    object.content = function () {
 
         return Object.values(this.items).join('');
     }
 }
 
-class DDPanel extends DDElementBind {
 
-    constructor(attribute = new DDAttribute(), items = {}, tag = 'div') {
+function DDPanel(object) {
 
-        super(attribute, tag);
-        this.items = items;
+    DDItems(object);
 
-    }
-
-    static fromContainer(jquery) {
+    object.fromContainer = function(jquery) {
 
         return jquery.children(this.identifier(true)).first();
-    }
-
-    inner() {
-
-       return Object.values(this.items).join('');
-    }
+    };
 
     /**
      * container
      * @param jquery
      */
-    setTo(jquery) {
+    object.setTo = function(jquery) {
 
-        this.constructor.fromContainer(jquery).remove();
+        this.fromContainer(jquery).remove();
         jquery.prepend(this.toString());
     }
 }
 
 
+//
+// class DDPanel extends DDElementBind {
+//
+//     constructor(bind = '', attribute = new DDAttribute(), items = {}, tag = 'div') {
+//
+//         super(bind, attribute, tag);
+//         this.items = items;
+//
+//     }
+//
+//     static fromContainer(jquery) {
+//
+//         return jquery.children(this.identifier(true)).first();
+//     }
+//
+//     inner() {
+//
+//        return Object.values(this.items).join('');
+//     }
+//
+//     /**
+//      * container
+//      * @param jquery
+//      */
+//     setTo(jquery) {
+//
+//         this.constructor.fromContainer(jquery).remove();
+//         jquery.prepend(this.toString());
+//     }
+// }
 
-class DDElementClick extends DDElement {
 
-    constructor(bind, content = '', attribute = new DDAttribute(), tag = 'div') {
-
-        super(content, attribute, tag);
-
-        attribute.associative('class')[bind] = bind;
-
-        this.handler = function (Jquery) {
-
-        };
-
-        var self = this;
-
-        DD.update.events['click' + this.bind()] = function () {
-
-            self.update();
-        };
-
-        this.update();
-    }
-
-    inner () {
-
-        return this.content;
-    }
-
-    update() {
-
-        var self = this;
-
-        $(this.bind(true)).off('click').click(function (e) {
-
-            self.handler(e);
-        });
-    }
-}
-
+//
+//
+// class DDElementClick extends DDElement {
+//
+//     constructor(bind, content = '', attribute = new DDAttribute(), tag = 'div') {
+//
+//         super(bind, content, attribute, tag);
+//
+//         attribute.associative('class')[bind] = bind;
+//
+//         this.handler = function (Jquery) {
+//
+//         };
+//
+//         var self = this;
+//
+//         DD.update.events['click' + this.bind()] = function () {
+//
+//             self.update();
+//         };
+//
+//         this.update();
+//     }
+//
+//     //
+//     // bind(selector = false) {
+//     //
+//     //     return selector ? '.' + this.$bind : this.$bind ;
+//     // }
+//
+//     inner () {
+//
+//         return this.content;
+//     }
+//
+//     update() {
+//
+//         var self = this;
+//
+//         $(this.bind(true)).off('click').click(function (e) {
+//
+//             self.handler(e);
+//         });
+//     }
+// }
+//
 
 
 const DD = new DDContainer();
-
 
 DD.init = function (selector) {
 
@@ -286,33 +415,70 @@ DD.update = {
     }
 };
 
-DD.panel.attribute.list('class');
 
-DD.panel.items['name'] = new DDElement('', 'Container');
-DD.panel.items['name'].attribute.list('class').push('pull-left');
-
-
-
-var click = DD.panel.items['close'] = new DDElementClick('DDShowHide');
-
-DD.panel.items['close'].attribute.list('class').push(
-    'glyphicon glyphicon-eye-close btn btn-default btn-xs pull-right'
-);
-
-click.handler = function(e) {
-
-    var click = $(e.target);
-    var container = DDContainer.from(click);
-    container.toggleClass('DDHide');
-    click.toggleClass('glyphicon-eye-close glyphicon-eye-open');
-};
-
-
-
-
-
-
-
+// DD.panel.attribute.list('class');
+//
+// DD.panel.items['name'] = function () {
+//
+//     var element = new DDElement('Container');
+//     element.attribute.list('class').push('pull-left');
+//     return element;
+// }();
+//
+//
+// DD.panel.items['show/hide'] = function () {
+//
+//     var click = new DDElementClick('DDShowHide');
+//
+//     click.attribute.list('class').push(
+//         'glyphicon glyphicon-eye-close btn btn-default btn-xs pull-right'
+//     );
+//
+//
+//     click.content = function() {
+//
+//
+//     }();
+//
+//     click.handler = function(e) {
+//
+//         var click = $(e.target);
+//         var container = DDContainer.from(click);
+//         container.toggleClass('DDHide');
+//         click.toggleClass('glyphicon-eye-close glyphicon-eye-open');
+//     };
+//
+//     return click;
+// }();
+//
+//
+//
+//
+//
+//
+//
+// DD.panel.items['new'] = function () {
+//
+//     var click = new DDElementClick('DDShowHide');
+//
+//
+//
+//     click.attribute.list('class').push(
+//         'glyphicon  glyphicon-plus btn btn-default btn-xs pull-left'
+//     );
+//
+//     click.handler = function(e) {
+//
+//         var click = $(e.target);
+//         var container = DDContainer.from(click);
+//         //container.toggleClass('DDHide');
+//        // click.toggleClass('glyphicon-eye-close glyphicon-eye-open');
+//     };
+//
+//     return click;
+//
+// }();
+//
 
 
 //
@@ -365,42 +531,54 @@ click.handler = function(e) {
 //
 //
 //
-// class DDModal extends DDElementAbstract {
-//
-//     constructor($attributes = {}) {
-//
-//         super($attributes);
-//
-//         this.attribute.list('class').push('modal fade DDAddModal');
-//         this.attribute.list('tabindex').push('-1');
-//         this.attribute.list('role').push('dialog');
-//         this.attribute.list('aria-labelledby').push('myModalLabel');
-//
-//         this.header = new DDItems;
-//         this.header.attribute.list('class').push('modal-header');
-//
-//         this.content = new DDItems;
-//         this.content.attribute.list('class').push('modal-body');
-//
-//         this.footer = new DDItems;
-//         this.footer.attribute.list('class').push('modal-footer');
-//     }
-//
-//     inner() {
-//         `
-//         <!-- Modal -->
-//             <div class="modal-dialog" role="document" style="width: 80%;">
-//                 <div class="modal-content">
-//
-//                     ${this.header}
-//                     ${this.item}
-//                     ${this.footer}
-//
-//                 </div>
-//             </div>
-//         `
-//     }
-// }
+class DDModal extends DDElementBind {
+
+    constructor(bind, $attributes = new DDAttribute()) {
+
+        super($attributes, 'div');
+
+        attribute.associative('class')[bind] = bind;
+
+        this.attribute.list('class').push(this.constructor.identifier());
+        this.attribute.list('class').push('modal fade');
+        this.attribute.list('tabindex').push('-1');
+        this.attribute.list('role').push('dialog');
+        this.attribute.list('aria-labelledby').push('myModalLabel');
+
+        this.header = new DDItems;
+        this.header.attribute.list('class').push('modal-header');
+
+        this.content = new DDItems;
+        this.content.attribute.list('class').push('modal-body');
+
+        this.footer = new DDItems;
+        this.footer.attribute.list('class').push('modal-footer');
+    }
+
+    show() {
+
+        $('body').append(this.toString());
+        $('.DDAddModalContent').html(DDNew.toString());
+    }
+
+    inner() {
+        `
+        <!-- Modal -->
+            <div class="modal-dialog" role="document" style="width: 80%;">
+                <div class="modal-content">
+
+                    ${this.header}
+                    ${this.item}
+                    ${this.footer}
+
+                </div>
+            </div>
+        `
+    }
+}
+
+
+
 // $(document).ready(function () {
 //
 //     $('body').append(`
