0f98b80e7dfffec43601f8b1eb82faca683ae4a3
ucing <you@example.com>
2018-10-03 12:19:08 +0700

    Remove Comment
    Remove Comment

--- js/dragdrop/element.js
+++ js/dragdrop/element.js
@@ -2,15 +2,17 @@
 
 
 
-function DDAttributeEditor (attrribute, value) {
+function DDAttributeEditor (attribute = '', value = '') {
 
     return `
-        <div class="input-group col-md-12">
-            <div class="col-md-2"><input value="${attrribute}" class="form-control" type="text"></div>
+        <div class="input-group col-md-12" style="margin-bottom: 5px; margin-top: 5px">
+            <div class="col-md-2"><input value="${attribute}" class="form-control" type="text"></div>
             <div class="col-md-8"><input value="${value}" class=" form-control" type="text"></div>
-            <div class="col-md-2"><button class="btn btn-danger">Remove</button></div>
+            <div class="col-md-2"><button class="btn btn-danger DDAttributeRemove">Remove</button></div>
         </div>`;
 }
+
+
 
 
 (function () {
@@ -23,6 +25,8 @@ function DDAttributeEditor (attrribute, value) {
 
 
             let container = DDContainer.fromInner($(e.target));
+
+            console.log(container);
 
             let attribute = container.attr('DDAttributeData');
             console.log(attribute);
@@ -39,13 +43,19 @@ function DDAttributeEditor (attrribute, value) {
 
 
             let modal = new DDModal('DDAttributeModal');
-            let element = new DDElement();
-            modal.content = element;
-            modal.content.attribute().list('class').push('DDAttributeContent');
 
 
-            let items  = new DDItems();
-            element.content = items;
+            modal.content = function () {
+
+                let items  = new DDItems();
+
+                for(let k in attribute) {
+
+                    items.items[k] = DDAttributeEditor(k, attribute[k]);
+                }
+
+                return items;
+            }();
 
             modal.header = `
                 <div class="col-md-2">Attribute</div>
@@ -53,13 +63,66 @@ function DDAttributeEditor (attrribute, value) {
                 <div class="col-md-2"><button class="btn btn-default DDAttributeAdd">Add</button></div>
             `;
 
-            for(let k in attribute) {
 
-                items.items[k] = DDAttributeEditor(k, attribute[k]);
-            }
+            modal.footer = function() {
+
+                let items = new DDItems();
+
+                items.items['cancel'] = function () {
+
+                    let click = new DDClick('DDAttributeCancel', function (e) {
+
+                        console.log('save');
+                    });
+
+                    click.element().content = 'Cancel';
+                    click.element().attribute().list('class').push('btn btn-default');
+                    click.element().attribute().list('data-dismiss').push('modal');
+
+                    return click.element();
+                }();
+
+
+                items.items['save'] = function () {
+
+                    let click = new DDClick('DDAttributeSave', function (e) {
+
+                        let buffer = [];
+
+                        $(e.target).parents('.modal-content').
+
+                        children('.modal-body').find('input').each(function (k, v) {
+                            buffer.push(v);
+                        });
+
+
+                        let attr = {};
+
+                        for(let i =0; i < buffer.length; i++) {
+
+
+                            attr[$(buffer[i]).val()] = $(buffer[i++]).val();
+                        }
+
+                        console.log(attr);
+                    });
+
+
+                    click.element().content = 'Save';
+                    click.element().attribute().list('class').push('btn btn-default');
+                   // click.element().attribute().list('data-dismiss').push('modal');
+
+                    return click.element();
+                }();
+
+                return items;
+
+            }();
+
+
+
 
             modal.show();
-
 
         });
 
@@ -70,11 +133,20 @@ function DDAttributeEditor (attrribute, value) {
 
 
 
-    let click = new DDClick('DDAttributeContent', function () {
+    new DDClick('DDAttributeAdd', function (e) {
 
+        console.log($(e.target).parents('.modal-content'));
+        console.log($(e.target).parents('.modal-content').children('.DDAttributeContent'));
+
+        $(e.target).parents('.modal-content').children('.modal-body').append(DDAttributeEditor());
+
+        DD.update.trigger();
     });
 
 
+    new DDClick('DDAttributeRemove', function (e) {
 
+        $(e.target).parents('.input-group').remove();
+    });
 
 })();
