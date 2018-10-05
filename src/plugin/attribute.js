5b69e747e80c5f45164626ac5d47140ee9ac7ce5
ucing <you@example.com>
2018-10-06 04:05:53 +0700

    Update
    - Refactor attribute structure

--- src/plugin/attribute.js
+++ src/plugin/attribute.js
@@ -1,138 +1,132 @@
+(function () {
 
+    function HtmeAttributeComponentEditor (attribute = '', value = '') {
 
-
-
-function HtmeAttributeComponentEditor (attribute = '', value = '') {
-
-    return `
+        return `
         <div class="input-group col-md-12" style="margin-bottom: 5px; margin-top: 5px">
             <div class="col-md-2"><input value="${attribute}" class="form-control" type="text"></div>
             <div class="col-md-9"><input value="${value}" class=" form-control" type="text"></div>
             <div class="col-md-1"><button class="btn btn-danger HtmeAttributeRemove">Remove</button></div>
         </div>`;
-}
+    }
 
 
-function HtmeGetAttributes(jquery) {
+    function HtmeGetAttributes(jquery) {
 
-    let attribute = {};
+        let attribute = {};
 
-    jquery.each(function() {
-        $.each(this.attributes, function() {
-            // this.attributes is not a plain object, but an array
-            // of attribute nodes, which contain both the name and value
-            if(this.specified) {
+        jquery.each(function() {
+            $.each(this.attributes, function() {
+                // this.attributes is not a plain object, but an array
+                // of attribute nodes, which contain both the name and value
+                if(this.specified) {
 
-                attribute[this.name] = this.value;
-            }
-        });
-    });
-
-    return attribute;
-}
-
-
-(function () {
-
-    let panel = new HtmeComponentPanel(Htme.menu);
-
-    let editor = function () {
-
-        let click = new HtmeComponentClick('HtmeComponentAttribute',function(e) {
-
-
-            let container = HtmeComponentBlock.binding().selectFromChildren($(e.target));
-
-
-            let attribute = HtmeGetAttributes(container);
-            let modal = new HtmeComponentModal('HtmeAttributeModal');
-
-
-            modal.content = function () {
-
-                let items  = new HtmeComponentItems();
-
-                for(let k in attribute) {
-
-                    items.items[k] = HtmeAttributeComponentEditor(k, attribute[k]);
+                    attribute[this.name] = this.value;
                 }
+            });
+        });
 
-                return items;
-            }();
+        return attribute;
+    }
 
-            modal.header = `
+
+
+    let modal = new HtmeComponentModal('HtmeAttributeModal');
+
+    modal.header = `
                 <div class="alert alert-warning" role="alert">Insert Warning Here</div>
                 <div class="col-md-2">Attribute</div>
                 <div class="col-md-9">Value</div>
                 <div class="col-md-1"><button class="btn btn-default HtmeAttributeAdd">Add</button></div>
             `;
 
+    modal.footer = function() {
 
-            modal.footer = function() {
+        let items = new HtmeComponentItems();
 
-                let items = new HtmeComponentItems();
+        items.items['close'] = function () {
 
-                items.items['close'] = function () {
+            let click = new HtmeComponentClick('HtmeAttributeClose', function (e) {
 
-                    let click = new HtmeComponentClick('HtmeAttributeClose', function (e) {
+                console.log('close');
+            });
 
-                        console.log('close');
-                    });
+            click.element().content = 'Close';
+            click.element().attribute().get('class').add('btn btn-default');
+            click.element().attribute().get('data-dismiss').add('modal');
 
-                    click.element().content = 'Close';
-                    click.element().attribute().get('class').add('btn btn-default');
-                    click.element().attribute().get('data-dismiss').add('modal');
-
-                    return click.element();
-                }();
-
-
-                items.items['save'] = function () {
-
-                    let click = new HtmeComponentClick('HtmeAttributeSave', function (e) {
-
-                        let buffer = [];
-
-                        $(e.target).parents('.modal-content').children('.modal-body').find('input').each(function (k, v) {
-
-                            buffer.push(v);
-
-                        });
-
-                        let attr = {};
-
-                        for(let i =0; i < buffer.length; i++) {
-
-                            attr[$(buffer[i]).val()] = $(buffer[++i]).val();
-                        }
-
-                        for(let k in attribute) {
-
-                            container.removeAttr(k);
-                        }
-
-                        for(let k in attr) {
-
-                            if(attr[k].length) {
-
-                                container.attr(k, attr[k]);
-                            }
-                        }
-                    });
+            return click.element();
+        }();
 
 
-                    click.element().content = 'Save';
-                    click.element().attribute().get('data-dismiss').add('modal');
-                    click.element().attribute().get('class').add('btn btn-default');
+        items.items['save'] = function () {
 
-                    return click.element();
-                }();
+            let click = new HtmeComponentClick('HtmeAttributeSave', function (e) {
 
-                return items;
+                let buffer = [];
 
-            }();
+                $(e.target).parents('.modal-content').children('.modal-body').find('input').each(function (k, v) {
+
+                    buffer.push(v);
+
+                });
+
+                let attr = {};
+
+                for(let i =0; i < buffer.length; i++) {
+
+                    attr[$(buffer[i]).val()] = $(buffer[++i]).val();
+                }
+
+                for(let k in attribute) {
+
+                    container.removeAttr(k);
+                }
+
+                for(let k in attr) {
+
+                    if(attr[k].length) {
+
+                        container.attr(k, attr[k]);
+                    }
+                }
+            });
+
+            click.element().content = 'Save';
+            click.element().attribute().get('data-dismiss').add('modal');
+            click.element().attribute().get('class').add('btn btn-default');
+
+            return click.element();
+        }();
+
+        return items;
+
+    }();
+
+
+    let editor = function () {
+
+        let click = new HtmeComponentClick('HtmeComponentAttribute',function(e) {
+
+            let container = HtmeComponentBlock.binding().selectFromChildren($(e.target));
+
+
+            let attribute = HtmeGetAttributes(container);
 
             modal.show();
+
+
+            let body = $('.HtmeAttributeModal').find('.modal-body');
+            body.empty();
+
+            let items  = new HtmeComponentItems();
+
+            for(let k in attribute) {
+
+                items.items[k] = HtmeAttributeComponentEditor(k, attribute[k]);
+            }
+
+            body.html(items.toString());
 
         });
 
@@ -144,7 +138,6 @@ function HtmeGetAttributes(jquery) {
     }();
 
 
-
     HtmeContainer.panel().menu('edit').submenus['element'] = editor;
     HtmeContent.panel().menu('edit').submenus['element'] = editor;
 
@@ -154,7 +147,6 @@ function HtmeGetAttributes(jquery) {
 
         Htme.update.trigger();
     });
-
 
     new HtmeComponentClick('HtmeAttributeRemove', function (e) {
 
